70b5937720af9a19caa9d8d99414fe11aee743f3
Dorian Somers <doriansomers@gmail.com>
2018-08-13 17:32:44 +0200

    add gh-pages-deploy

--- src/app/wizard/accesspoint/index.js
+++ src/app/wizard/accesspoint/index.js
@@ -1,6 +1,5 @@
 export * from './accesspoint.controller';
 import prompted_entry1 from './prompted_entry1.html';
 import prompted_entry2 from './prompted_entry2.html';
-import ap_final from './ap_final.html';
 
-export { prompted_entry1, prompted_entry2, ap_final }
+export { prompted_entry1, prompted_entry2 }
