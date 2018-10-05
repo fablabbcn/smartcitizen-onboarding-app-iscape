6505915689436b7769521fdd9dde54c366e78b29
Pral2a <guillem@pral2a.com>
2018-10-05 20:45:04 +0200

    Fixes navigation issues

--- src/app/wizard/wizard.controller.js
+++ src/app/wizard/wizard.controller.js
@@ -44,7 +44,7 @@ export function wizardController($scope, $location, $sce, $window, $timeout, Seg
     $scope.seque = function() {
 
         if ($scope.segueControl == 'ready') {
-          console.log($scope.payload.template);
+          console.log('Next state:', $scope.payload.template);
             switch ($scope.payload.template) {
                 case 'handshake':
                     if ($scope.handShakeState == false) {
@@ -93,7 +93,6 @@ export function wizardController($scope, $location, $sce, $window, $timeout, Seg
         AnimationService.leaving(true);
         $scope.payload.progressShow = 'blue';
         $timeout(function() {
-            console.log('Next slide:', $scope.payload.index);
             SegueService.nextPage($scope.pre_made, nextState);
             $window.scrollTo(0, 0);
             $scope.payload.progressShow = ' ';
